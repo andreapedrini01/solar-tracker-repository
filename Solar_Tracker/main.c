92221225397070bdc6c48d0a018988c63434c721
Irina <airinlavis@gmail.com>
2023-12-11 11:33:02 +0100

    Added map function

--- Solar_Tracker/main.c
+++ Solar_Tracker/main.c
@@ -71,6 +71,10 @@ void _hwInit()
     init_motors();
 }
 
+int map(int x, int in_min, int in_max, int out_min, int out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void readAndMove() {
 
    int sensorValues[numSensors];
